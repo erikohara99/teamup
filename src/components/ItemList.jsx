72c2f3f4eca48352be37fb4d746314b3babd8b73
Erik <slicedblue@gmail.com>
2021-07-25 18:52:02 -0400

    removed unneccessary components and nested code inside parent components

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ItemCard from './ItemCard';
 
 const ItemList = (props) => {
+
+    const {onSelect} = props;
+
     return ( 
         <div className="card-container">
             {props.items.map(item => {
-                return (
-                    <ItemCard key={item.key} item={item} onSelect={props.onSelect}/>
+                return(
+                    <img id="card" src={`http://ddragon.leagueoflegends.com/cdn/11.15.1/img/item/${item.image.full}`} onClick={() => {onSelect(item)}}></img>
                 );
             })}
         </div>
