f0f3b1e6cd1867c68e5eb4c2d68a46886949e382
Erik <slicedblue@gmail.com>
2021-07-25 19:11:46 -0400

    added missing icon if champ icon cannot be loaded

--- src/components/ChampList.jsx
+++ src/components/ChampList.jsx
@@ -7,6 +7,7 @@ const ChampList = (props) => {
     return ( 
         <div className="card-container">
             {props.champs.map(champion => {
+
                 return(
                     <img id="card" src={`http://ddragon.leagueoflegends.com/cdn/11.14.1/img/champion/${champion.image.full}`} onClick={() => {onSelect(champion)}}></img>
                 );
